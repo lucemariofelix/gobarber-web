f24652a09e8459af44c814c6f8cbe4713c31613b
Lucemario Felix <jlucemariof@gmail.com>
2020-04-23 17:48:36 -0300

    estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App: React.FC = () => <h1>Olá Gobarber Web</h1>;
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <h1>Olá Gobarber Web</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
