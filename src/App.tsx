363a6c2f725d2012c43d38ba515d07aeadb5e244
Lucemario Felix <jlucemariof@gmail.com>
2020-04-25 00:08:07 -0300

    login pelo contexto

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 
-import Authcontext from './context/Authcontext';
+import { AuthProvider } from './context/Authcontext';
 
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
 const App: React.FC = () => (
   <>
-    <Authcontext.Provider value={{ name: 'Lucemario' }}>
+    <AuthProvider>
       <SignIn />
-    </Authcontext.Provider>
+    </AuthProvider>
 
     <GlobalStyle />
   </>
