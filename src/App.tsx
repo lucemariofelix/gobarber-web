6bc3090b2351ff64c1818dd4ce229d3a69340801
Lucemario Felix <jlucemariof@gmail.com>
2020-04-24 23:30:07 -0300

    API de contexto

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 
+import Authcontext from './context/Authcontext';
+
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <Authcontext.Provider value={{ name: 'Lucemario' }}>
+      <SignIn />
+    </Authcontext.Provider>
+
     <GlobalStyle />
   </>
 );
